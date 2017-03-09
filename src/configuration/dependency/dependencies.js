213ea10c9f4f841330bd6f50ed61a95af6d32e67
Danilo Araújo Silva <silva.danilo.araujo@gmail.com>
2017-03-09 18:35:58 -0300

    All components.

--- src/configuration/dependency/dependencies.js
+++ src/configuration/dependency/dependencies.js
@@ -5,8 +5,8 @@ window.$ = window.jQuery = $;
 import 'configuration/event/tap/apple/ios/react-tap-event-plugin';
 
 import 'configuration/ui/material-components-web/material-components-web';
-//import 'configuration/ui/material-design-lite/material-design-lite';
+import 'configuration/ui/material-design-lite/material-design-lite';
 import 'configuration/ui/materialize-css/materialize-css';
-//import 'configuration/ui/vue/vue';
+import 'configuration/ui/vue/vue';
 
 require('configuration/ui/semantic-ui/semantic-ui');
